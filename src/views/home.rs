24775e65176ba1f44dddd90adb68ced4d961903d
haruki7049 <tontonkirikiri@gmail.com>
2026-04-13 11:50:38 +0900

    Update

--- src/views/home.rs
+++ src/views/home.rs
@@ -3,5 +3,12 @@ use dioxus::prelude::*;
 /// The Home page component that will be rendered when the current route is `[Route::Home]`
 #[component]
 pub fn Home() -> Element {
-    rsx! {}
+    rsx! {
+        p { "Hello, Dioxus and Haruki7049!!" }
+        p { "This is an example-counter-app page." }
+        a {
+            href: "https://github.com/haruki7049/example-counter-app",
+            "github.com/haruki7049/example-counter-app",
+        }
+    }
 }
